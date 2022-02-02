c253804f1f54e80302493d8463d7e99582948311
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2022-02-02 12:30:52 +0700

    add test

---
+++ test.js
@@ -0,0 +1,14 @@
+function calculateRandomNumber() {
+    return Math.random();
+}
+
+// find the sum of two numbers
+function sum(a, b) {
+    return a + b;
+}
+
+// find word without test string
+// and return the word
+function findWord(word) {
+    return word.replace("test", "");
+}   // end of findWord      
